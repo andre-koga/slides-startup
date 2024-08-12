838d840723d9f612e3ca20c30961aff0e4a7d2dc
S Taubman <akoga3@gatech.edu>
2024-08-12 18:42:09 -0400

    tailwind aint workin

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	// Styles
+	import '../app.css';
+</script>
+
+<div>
+	<!-- <Header /> -->
+
+	<main class="relative z-0 min-h-screen">
+		<slot />
+	</main>
+
+	<!-- <Footer /> -->
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
-    import { processMarkup } from "$lib/parser";
-    import { generateHTML } from "$lib/htmlGenerator";
-	import { type Slideshow } from "$lib/types";
+	import { processMarkup } from '$lib/parser';
+	import { generateHTML } from '$lib/htmlGenerator';
+	import { type Slideshow } from '$lib/types';
 
-    let markup = `template=test.template\n---Slide 1\n# A great day\nHello there\n---Slide 2\nAnother great day`;
-    let slideshow : Slideshow = processMarkup(markup);
+	let markup = `template=test.template\n---Slide 1\n# A great day\nHello there\n---Slide 2\nAnother great day`;
+	let slideshow: Slideshow = processMarkup(markup);
 
-    const onInput = () => {
-        slideshow = processMarkup(markup);
-        console.log(slideshow);
-    } 
+	const onInput = () => {
+		slideshow = processMarkup(markup);
+		console.log(slideshow);
+	};
 </script>
 
-<h1>Flow</h1>
-<textarea bind:value={markup} on:input={onInput} style="width: 200px; height: 200px; resize: none"/>
+<h1 class="bg-blue-500">Flux</h1>
+<textarea
+	bind:value={markup}
+	on:input={onInput}
+	style="width: 200px; height: 200px; resize: none"
+/>
 <div>
-    {#each slideshow.slides as slide}
-        <div style="border: 1px solid black">
-            {@html generateHTML(slide)}
-        </div>
-    {/each}
+	{#each slideshow.slides as slide}
+		<div style="border: 1px solid black">
+			{@html generateHTML(slide)}
+		</div>
+	{/each}
 </div>
+
+<div class="text-red-400">yey</div>
