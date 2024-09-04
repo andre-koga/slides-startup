db8459de52cd99ddc67e1e53f6d3a30ba4f5ed1d
Sammy Taubman <staubman1@gmail.com>
2024-09-04 10:56:00 -0400

    Update cursor logic to use textarea

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,52 +32,13 @@ function handleTextareaInput(event: Event) {
     initialText = target.value;
     lines = initialText.split('\n');
   }
-
-  function checkCursorPosition(textarea: HTMLTextAreaElement) {
-	const cursorIndex = textarea.value.slice(0, textarea.selectionStart).length;
-	// position the cursor based on the cursorIndex
-	const textBeforeCursor = textarea.value.slice(0, cursorIndex);
-	const textBeforeCursorLines = textBeforeCursor.split('\n');
-	const lastLine = textBeforeCursorLines[textBeforeCursorLines.length - 1];
-	const lastLineIndex = textBeforeCursorLines.length - 1;
-	const lastLineLength = lastLine.length;
-	const lastLineIndexLength = lastLineIndex.toString().length;
-	const lastLineIndexWidth = lastLineIndexLength * 8;
-	const lastLineTextWidth = lastLineLength * 8;
-	const cursorLeft = lastLineIndexWidth + lastLineTextWidth;
-	const cursorTop = lastLineIndex * 20;
-
-	const yOffset = 10;
-	const xOffset = 46;
-	cursorPosition = { top: cursorTop + yOffset, left: cursorLeft + xOffset };
-  }
-
-  function handleOnClick(event: MouseEvent) {
-	const target = event.target as HTMLTextAreaElement;
-	checkCursorPosition(target);
-  }
-
-  function handleKeyUp(event: KeyboardEvent) {
-    const target = event.target as HTMLTextAreaElement;
-    checkCursorPosition(target);
-  }
-
-  onMount(() => {
-    const textarea = document.querySelector('textarea');
-    if (textarea) {
-      checkCursorPosition(textarea as HTMLTextAreaElement);
-    }
-  });
-
 </script>
 
 <div class="relative grid grid-cols-2 items-stretch gap-1 overflow-hidden p-1">
 	<text-editor class="relative rounded-lg bg-slate-100 dark:bg-slate-800 overflow-hidden border-4 border-slate-300 dark:border-slate-700 flex flex-col font-jet">
 		
 		<!-- make textarea cover the exact dimensions of the text-editor and make it invisible but still interactable -->
-		<textarea bind:value={initialText} on:input={handleTextareaInput} on:keyup={handleKeyUp} on:click={handleOnClick} class="opacity-10 text-sm absolute pl-1.5 top-0 pr-1.5 font-jet pt-[0.5rem] bottom-0 left-12 right-0 text-black z-50"></textarea>
-
-		<div class="fake-cursor" style="top: {cursorPosition.top}px; left: {cursorPosition.left}px;"></div>
+		<textarea bind:value={initialText} on:input={handleTextareaInput} class="text-[#00000000] bg-[#00000000] caret-black dark:caret-slate-200 text-sm absolute pl-1.5 top-0 pr-1.5 font-jet pt-[0.5rem] bottom-0 left-12 right-0 z-50"></textarea>
 
 		<div class="absolute left-0 h-full w-12 dark:bg-slate-600 z-10"/>
 		 
