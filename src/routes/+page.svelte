ac2940e053a59f0737ea3c5ff759787813796b6e
S Taubman <akoga3@gatech.edu>
2024-09-08 13:33:58 -0400

    better styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,18 +36,18 @@ let slideshow: Slideshow = processMarkup(initialText);
 </script>
 
 <div class="relative grid grid-cols-2 items-stretch gap-1 overflow-hidden p-1">
-	<text-editor class="relative rounded-lg bg-slate-100 dark:bg-slate-800 overflow-hidden border-4 border-slate-300 dark:border-slate-700 flex flex-col font-jet">
+	<text-editor class="relative rounded-lg bg-slate-50 dark:bg-slate-800 overflow-hidden border-4 border-slate-300 dark:border-slate-700 flex flex-col font-jet">
 		
 		<!-- make textarea cover the exact dimensions of the text-editor and make it invisible but still interactable -->
-		<textarea bind:value={initialText} on:input={handleTextareaInput} class="text-[#00000000] bg-[#00000000] caret-black dark:caret-slate-200 text-sm absolute pl-1.5 top-0 pr-1.5 font-jet pt-[0.5rem] bottom-0 left-12 right-0 z-50"></textarea>
+		<textarea bind:value={initialText} on:input={handleTextareaInput} class="text-[#00000000] focus:outline-none hover:outline-none bg-[#00000000] active:border-none caret-black dark:caret-slate-200 text-sm absolute pl-1.5 top-0 pr-1.5 font-jet pt-[0.5rem] bottom-0 left-12 right-0 z-50"></textarea>
 
-		<div class="absolute left-0 h-full w-12 dark:bg-slate-600 z-10"/>
+		<div class="absolute left-0 h-full w-12 dark:bg-slate-700 bg-slate-300 z-10"/>
 		 
 		<!-- for each line in lines, generate a line with a number on the left side displaying the index + 1 -->
 		<div class="pt-2 z-20">
 		{#each lines as line, index}
 			<div class="editor-line grid font-jet items-stretch" id={`line-${index}`}>
-				<div class="editor-line-number dark:text-slate-400 dark:bg-slate-600 text-right px-2">{index + 1}</div>
+				<div class="editor-line-number text-right px-2 text-slate-500">{index + 1}</div>
 				<div class="editor-line-text dark:text-slate-200 px-1.5">{line}</div>
 			</div>
 		{/each}
@@ -80,4 +80,12 @@ let slideshow: Slideshow = processMarkup(initialText);
     background-color: white;
     pointer-events: none;
   }
+
+textarea::-moz-selection { /* Code for Firefox */
+  @apply bg-slate-300;
+}
+
+textarea::selection {
+	@apply dark:bg-slate-700 bg-slate-300 text-black dark:text-white;
+}
 </style>
